999f343117450d39293d4de6fcfd516b7a67024d
Danilo Pinheiro <dan.augusto@gmail.com>
2012-04-24 17:05:41 -0300

    adicionando arquivos do projeto

---
+++ HolyTrip/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HolyTrip.Models;
+using System.Web.Helpers;
+namespace HolyTrip.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public JsonResult People()
+        {
+            List<Person> personItems = new List<Person>();
+            Person person = new Person();
+            person.Name = "Zé Berdeu";
+            person.Occupation = "Capataz";
+            person.Age = 25;
+            personItems.Add(person);
+
+            person = new Person();
+            person.Name = "Chico Bento";
+            person.Occupation = "Fazendeiro";
+            person.Age = 30;
+            personItems.Add(person);
+
+            People people = new People();
+            people.Items = personItems;
+
+            return Json(people, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HolyTrip/Models/PeopleModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolyTrip.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Occupation { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class People
+    {
+        public List<Person> Items { get; set; }
+    }
+}
